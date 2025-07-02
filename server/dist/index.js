785950f27fb865cfb98c483494752b201b0f97cb
Aditi <aditi.mondal@cyberswift.com>
2025-07-02 15:35:48 +0530

    fixed files to run smoothly

---
+++ server/dist/index.js
@@ -0,0 +1,33 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const pg_1 = require("pg");
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const pool = new pg_1.Pool({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: Number(process.env.PGPORT),
+});
+app.get('/api', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { rows } = yield pool.query('SELECT NOW()');
+    res.json({ now: rows[0].now });
+}));
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`API listening on http://localhost:${PORT}`);
+});
